9993be52042ac2412f40ad3901f9b8c30e78b160
Diego Morales <diemoret17@gmail.com>
2025-10-07 21:30:15 -0600

    test aprobado

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+const hash = require('./testindex');
+
+test('Verificar hash SHA-256', () => {
+    const mensaje = 'contraseña';
+    const hashtest = hash(mensaje);
+    expect(hashtest).toEqual('edf9cf90718610ee7de53c0dcc250739239044de9ba115bb0ca6026c3e4958a5');
+});
+
